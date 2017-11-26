6061a1606d086ee309fd92cbc542e30fa3fb3e97
nOOBIE nOOBIE <a.u.aua937@gmail.com>
2017-11-26 17:35:46 +0545

    Add files via upload

---
+++ nepalinews/nepalinews/spiders/onlinekhabr.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class OnlinekhabrSpider(scrapy.Spider):
+    name = "onlinekhabr"
+    allowed_domains = ["onlinekhabar.com/content/news"]
+    start_urls = ('https://onlinekhabar.com/content/news/',)
+
+    def parse(self, response):
+        links=response.xpath('//*[@class="news_loop"]/h2/a/@href').extract()
+        for link in links:
+        	yield{"link":link}
+        nextpageurl = response.xpath('//*[@class="next page-numbers"]/@href').extract_first()
+        absolute_next_page_url = response.urljoin(nextpageurl)    
+        yield scrapy.Request(absolute_next_page_url,dont_filter=True)
+
+
